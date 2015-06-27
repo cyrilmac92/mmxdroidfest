e67e84c1448af559676883e231618744215ba37e
unknown <philic3@INITPHILIC3L1C.corp.emc.com>
2015-06-27 14:11:50 +0530

    Initial commit

---
+++ src/main/java/mmx/com/hack/RestoreBackupApplication.java
@@ -0,0 +1,12 @@
+package mmx.com.hack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestoreBackupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestoreBackupApplication.class, args);
+    }
+}
